bfc8fca7d99721e039dff7d955470d9f4049b581
escuela@davinci.edu.ar <felipe.yaschischin@davinci.edu.ar>
2022-11-14 13:45:48 -0300

    feat: add objects references

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import
+import transporte.MedioDeTransporte;
+
 public class Main {
     public static void main(String[] args){
 
+        ModoDeTransporte camionDisponible = new ModoDeTransporte (
+                500,0
+                new Dimension( 2.5 , 1.5 , 4)
+        );
+        ObjetosTransportables silla = new ObjetosTransportables(
+                "Mesa de Madera",
+                2.0,
+                new Dimension(1.4,1,0.2),
+
+
+        );
     }
 }
 
